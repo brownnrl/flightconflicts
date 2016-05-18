bd1b8ef6ae287dd528d769dce4f3b91d6e8e365f
eamoncaddigan <eamon.caddigan@gmail.com>
2016-05-18 09:45:33 -0400

    Description and beginning documentation.

---
+++ R/flightconflicts.R
@@ -0,0 +1,8 @@
+#' flightconflicts: Analyze conflicts between aircraft
+#' 
+#' This package contains tools to analyze aircraft trajectories and identify
+#' loss of well-clear (LoWC) and near mid-air collisions (NMAC).
+#' 
+#' @docType package
+#' @name flightconflicts
+NULL
