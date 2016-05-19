377ca358eb1367770dada9b3d0c65a91534b8272
eamoncaddigan <eamon.caddigan@gmail.com>
2016-05-19 16:24:15 -0400

    Identifying NMACs.

---
+++ R/identifyNMAC.R
@@ -0,0 +1,33 @@
+#' Determine whether an NMAC (near mid air collision) has occurred at each time
+#' point.
+#'
+#' @param trajectory1 A matrix or SpatialPoints object indicating the trajectory
+#'   of the first aircraft.
+#' @param trajectory2 A matrix or SpatialPoints object indicating the trajectory
+#'   of the second aircraft.
+#' @return The logical vector indicating whether NMAC criteria are met at each
+#'   time point.
+#'
+#' @details NMAC is a cylinder +/- 100 ft above and below the ownship with a
+#'   radius of 500 ft.
+#'
+#' @export
+identifyNMAC <- function(trajectory1, trajectory2) {
+  if (!is.flighttrajectory(trajectory1) || !is.flighttrajectory(trajectory2)) {
+    stop("Both arguments must be instances of flighttrajectory")
+  }
+  if (!isTRUE(all.equal(trajectory1$timestamp, trajectory2$timestamp))) {
+    stop("Both trajectories must have the same time stamps")
+  }
+
+  horizontalDistanceFt <- geosphere::distHaversine(cbind(trajectory1$longitude,
+                                                         trajectory1$latitude),
+                                                   cbind(trajectory2$longitude,
+                                                         trajectory2$latitude),
+                                                   r = 20925646)
+  verticalDistanceFt <- abs(trajectory1$altitude - trajectory2$altitude)
+
+  isNMAC <- (horizontalDistanceFt < 500) | (verticalDistanceFt < 100)
+
+  return(isNMAC)
+}
