3ae38668544e31a25af45d80449e87ab9cae1869
eamoncaddigan <eamon.caddigan@gmail.com>
2016-09-14 11:36:23 -0400

    More tests for calculateTCPA

--- tests/testthat/test_calculateTCPA.R
+++ tests/testthat/test_calculateTCPA.R
@@ -6,6 +6,8 @@ library(geosphere)
 kacy <- c(-74.5771667, 39.4575833)
 kphl <- c(-75.2408658, 39.8722494)
 
+meterPer200Knots <- 0.009719
+
 test_that("correct tCPA for head-on collision", {
   trajectory1 <- createTrajectory(kacy[1], kacy[2], 
                                   bearing = bearingRhumb(kacy, kphl),
@@ -15,6 +17,22 @@ test_that("correct tCPA for head-on collision", {
                                   groundspeed = 200)
   
   expect_equal(calculateTCPA(trajectory1, trajectory2),
-               distGeo(kacy, kphl)/2 * 0.009719,
+               distGeo(kacy, kphl)/2 * meterPer200Knots,
                tolerance = 1)
+})
+
+test_that("TCPA for actual trajectories", {
+  numPoints <- round(distGeo(kacy, kphl) * meterPer200Knots)
+  coords <- gcIntermediate(kacy, kphl,
+                           n = numPoints)
+  trajectory1 <- createTrajectory(coords[, "lon"], coords[, "lat"])
+  trajectory2 <- createTrajectory(rev(coords[, "lon"]), rev(coords[, "lat"]))
+  tcpaVector <- calculateTCPA(trajectory1, trajectory2)
+  
+  expect_equal(tcpaVector[seq(ceiling(numPoints/2)+1, numPoints)],
+               0 * seq(ceiling(numPoints/2)+1, numPoints))
+  
+  expect_equal(diff(tcpaVector[seq(1, floor(numPoints/2))]), 
+               -1*diff(seq(1, floor(numPoints/2))),
+               tolerance = 1e-3)
 })
